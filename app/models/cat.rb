7a59a1e1e6c6dfcfb813cca768ce75c427aef91f
Cyrielle Lemeunier <dev@lemeunier.org>
2016-05-17 10:57:51 +0200

    delete validates on cat, validates on reservation

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,9 +1,4 @@
 class Cat < ActiveRecord::Base
   belongs_to :user
   has_many :reservations
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :price, presence: true
-  validates :cat, presence: true
-  validates :user, presence: true
 end
