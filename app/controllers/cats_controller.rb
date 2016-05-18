ea0ce54a1da203928b4c35a4f114682cd68938a8
Alice Gillet <alice.vaismangillet@gmail.com>
2016-05-18 13:02:53 +0200

    wip sql guery

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,18 @@
 class CatsController < ApplicationController
+
+  def index
+    @users = User.where(city: params[:city])
+    raise
+    puts @users
+    # @cats = Cat.joins(:users).where(users: { city: params[:city] })
+    # (city: Cat.user.city = "Paris")
+    # redirect_to cats_path(@cats)
+  end
+
+  private
+
+  def cat_params
+    params.permit(:city)
+  end
+
 end
