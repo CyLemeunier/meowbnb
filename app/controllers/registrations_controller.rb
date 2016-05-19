3aebdfd527683c32e47a85f3a32621a204b5cace
Yohan Belmin <ybelmin@hotmail.com>
2016-05-19 17:12:59 +0200

    Corrected Sign Up & Edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  #Redefining these methods to use Log In form in Cats pages
-  helper_method :resource_name, :resource, :devise_mapping
-
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-  #End of Devise methods
+  #DO NOT Redefe helper methods in this Controller
+  #Otherwise, Sign up and Edit don't work anymore
 
   private
 
