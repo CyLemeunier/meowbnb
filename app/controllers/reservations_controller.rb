98b887c0242413e48b7e37b3a4500d6e259b4f91
Cyrielle Lemeunier <dev@lemeunier.org>
2016-05-19 11:23:00 +0200

    rédaction des def new et create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,15 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :price)
+  end
 end
