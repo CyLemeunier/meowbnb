b640bedca1e3333fcd92eb483a1a5db85f474f57
Cyrielle Lemeunier <dev@lemeunier.org>
2016-05-20 14:40:29 +0200

    écriture du update associé au redirect_to

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -24,6 +24,20 @@ class ReservationsController < ApplicationController
     end
   end
 
+  def edit
+    @cat = Cat.find(params[:cat_id])
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.find(params[:cat_id])
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_update_params)
+      redirect_to cat_reservations_path(@cat)
+    end
+
+  end
+
   #Redefining these methods to use Log In form in Cats pages
   helper_method :resource_name, :resource, :devise_mapping
 
@@ -44,6 +58,10 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date)
+  end
+
+  def reservation_update_params
+    params.require(:reservation).permit(:user_review, :user_rating)
   end
 
 end
